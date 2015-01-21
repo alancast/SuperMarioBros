d43fc7c6ed32ad3deef72d8cc87eef5a62491667
shmacks <mdyinger@umich.edu>
2015-01-21 14:26:47 -0500

    hitBox
    made hitBox

--- Assets/Scripts/Mario_spriteController.cs
+++ Assets/Scripts/Mario_spriteController.cs
@@ -5,13 +5,11 @@ public class Mario_spriteController : MonoBehaviour {
 
 	Animator anim;
 	PE_Obj marioPhys;
-	PE_Obj ground;
 
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator> ();
 		marioPhys = GetComponent<PE_Obj> ();
-		ground = marioPhys.ground;
 	}
 	
 	// Update is called once per frame
---
+++ Assets/Scripts/hitBlockCollision.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hitBlockCollision : MonoBehaviour {
+
+	Animator anim;
+	bool wasHit = false;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		wasHit = true;
+		anim.SetBool ("wasHit", wasHit);
+	}
+
+}
