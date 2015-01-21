27d286e36a315cac47b15bcfdb1cd2584d367870
shmacks <mdyinger@umich.edu>
2015-01-21 14:48:17 -0500

    fixed hitblock collision direction

--- Assets/Scripts/hitBlockCollision.cs
+++ Assets/Scripts/hitBlockCollision.cs
@@ -20,7 +20,7 @@ public class hitBlockCollision : MonoBehaviour {
 		PE_Dir dir = other.gameObject.GetComponent<PE_Obj> ().dir;
 		//print ("goo");
 
-		if (dir == PE_Dir.down || dir == PE_Dir.downLeft ||  dir == PE_Dir.downRight) {
+		if (dir == PE_Dir.up || dir == PE_Dir.upLeft ||  dir == PE_Dir.upRight) {
 				wasHit = true;
 				anim.SetBool ("wasHit", wasHit);
 		}
