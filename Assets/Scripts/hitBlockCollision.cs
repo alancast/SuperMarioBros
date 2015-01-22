d1580f18a03996cf8abe81594071ab08861ced85
Alex <alancast@umich.edu>
2015-01-22 01:13:11 -0500

    pushing for the night

--- Assets/Scripts/hitBlockCollision.cs
+++ Assets/Scripts/hitBlockCollision.cs
@@ -5,24 +5,31 @@ public class hitBlockCollision : MonoBehaviour {
 
 	Animator anim;
 	bool wasHit = false;
+	public float 		raycastDistance;
 
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator> ();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	bool onTop(Collider other){
+		if (Physics.Raycast(other.transform.position, 
+		                    new Vector3(0, 1, 0), 
+		                    other.transform.collider.bounds.size.y + raycastDistance)) return true;
+		else return false;
 	}
 
 	void OnTriggerEnter(Collider other) {
 		PE_Dir dir = other.gameObject.GetComponent<PE_Obj> ().dir;
 		//print ("goo");
 
-		if (dir == PE_Dir.up) {
-				wasHit = true;
-				anim.SetBool ("wasHit", wasHit);
+//		if (dir == PE_Dir.up) {
+//				wasHit = true;
+//				anim.SetBool ("wasHit", wasHit);
+//		}
+		if(onTop(other)){
+			wasHit = true;
+			anim.SetBool ("wasHit", wasHit);
 		}
 	}
 
