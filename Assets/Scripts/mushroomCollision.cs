94244862ce62aadcce1fbe28f2dd5eca43e7ac38
shmacks <mdyinger@umich.edu>
2015-01-27 00:55:42 -0500

    working on shroom and troopa

---
+++ Assets/Scripts/mushroomCollision.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mushroomCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "Player") {		
+			PE_Obj thisShroom = this.GetComponent<PE_Obj> ();
+		
+			PhysicsEngine.objs.Remove (thisShroom);
+		
+			Destroy (this.gameObject);
+		}
+	}
+}
