b057d88fe24c5cd5b867041757f2ca8c6c3026f0
Alex <alancast@umich.edu>
2015-01-27 18:46:53 -0500

    don't know

---
+++ Assets/Scripts/coinCollision (1).cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class coinCollision : MonoBehaviour {
+
+
+	void OnTriggerEnter(Collider other) {
+
+		PE_Obj thisCoin = this.GetComponent<PE_Obj> ();
+
+		PhysicsEngine.objs.Remove(thisCoin);
+
+		//PhysicsEngine.objs.RemoveAt (coinIndex);
+
+		Destroy (this.gameObject);
+	}
+
+		
+
+}
