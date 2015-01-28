a5c0456fa170f5835a59b15cb2043e60378d1b15
shmacks <mdyinger@umich.edu>
2015-01-27 22:37:37 -0500

    mario states now Seen!

--- Assets/Scripts/Mario_spriteController.cs
+++ Assets/Scripts/Mario_spriteController.cs
@@ -5,16 +5,28 @@ public class Mario_spriteController : MonoBehaviour {
 
 	Animator anim;
 	PE_Obj marioPhys;
+	BoxCollider marioCollider;
+	Vector3 bigColliderDim = new Vector3(1f, 1.6f, 1f);
+	Vector3 smallColliderDim = new Vector3(.85f, 1f, 1f);
+	MarioState state;
 
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator> ();
 		marioPhys = GetComponent<PE_Obj> ();
+		marioCollider = GetComponent<BoxCollider> ();
+		state = GetComponent<PE_Controller> ().state;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		Movement ();
+		state = GetComponent<PE_Controller> ().state;
+		if (state == MarioState.Small) {
+			marioCollider.size = smallColliderDim;
+		} else {
+			marioCollider.size = bigColliderDim;
+		}
 	}
 
 	void Movement (){
