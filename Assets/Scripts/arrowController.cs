7bc818f68b7d07307d450666e1d160c39709197a
shmacks <mdyinger@umich.edu>
2015-02-04 22:56:59 -0500

    added shtuff

--- Assets/Scripts/arrowController.cs
+++ Assets/Scripts/arrowController.cs
@@ -9,13 +9,11 @@ public class arrowController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator> ();
-		GameObject mario = GameObject.FindGameObjectWithTag ("Player");
-		mario_Phys = mario.GetComponent<PE_Obj> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float h_vel = Mathf.Abs(mario_Phys.vel.x);
+		float h_vel = Mathf.Abs(PE_Controller.instance.vel.x);
 		anim.SetFloat ("h_vel", h_vel);
 	}
 }
