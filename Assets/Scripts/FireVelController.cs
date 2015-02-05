40b4ba11e49512ad81f6ef7232450852a3f5b9e2
shmacks <mdyinger@umich.edu>
2015-02-04 20:06:43 -0500

    fixed plant yo

--- Assets/Scripts/FireVelController.cs
+++ Assets/Scripts/FireVelController.cs
@@ -4,17 +4,27 @@ using System.Collections;
 public class FireVelController : MonoBehaviour {
 	PE_Obj this_FireBall;
 	public Vector3 fireSpeed;
+	float startTime;
+	public float lifeTime = .08f;
 
 
 	// Use this for initialization
 	void Start () {
 		this_FireBall = GetComponent<PE_Obj> ();
 		//this_FireBall.acc = Vector3.zero;
+		startTime = Time.time;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		//this_FireBall.acc = Vector3.zero;
 		this_FireBall.vel = fireSpeed;
+
+		if((startTime - Time.time) > lifeTime){
+
+			PE_Obj thisFB = this.GetComponent<PE_Obj> ();
+			PhysicsEngine.objs.Remove(thisFB);
+			Destroy (this.gameObject);
+		}
 	}
 }
--- Assets/Scripts/plantShot.cs
+++ Assets/Scripts/plantShot.cs
@@ -11,38 +11,55 @@ public class plantShot : MonoBehaviour {
 	Vector3 shotOrigin;
 	public GameObject FireBall;
 	public float speedConstant = 1f;
-
+	public float minDist = 3f;
+	Animator plant_anim;
+	bool alreadyShot = false;
 
 
 	// Use this for initialization
 	void Start () {
-		startTime = Time.time;
 		mario = GameObject.FindGameObjectWithTag ("Player");
 		shotOrigin = this.transform.position;
 		shotOrigin.y += yOffsetForShot;
+		plant_anim = GetComponentInChildren<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		if (Time.time - startTime > timeBetweenShots) {
-			startTime = Time.time;
+		if (plant_anim.GetCurrentAnimatorStateInfo (0).IsName ("plant_Top")) {
 
-			if(Mathf.Abs(mario.transform.position.x - this.transform.position.x) < shotDistance){
-				GameObject curFireBall = (GameObject)Instantiate(FireBall, shotOrigin, Quaternion.identity);
-				FireVelController _FireBall = curFireBall.GetComponent<FireVelController>();
+						if (Mathf.Abs (PE_Controller.instance.transform.position.x - this.transform.position.x) < shotDistance 
+								&& Mathf.Abs (PE_Controller.instance.transform.position.y - this.transform.position.y) < (shotDistance - 3)
+			    				&& !alreadyShot) {
 
-				Vector3 fireVel = mario.transform.position;
-				fireVel.y += 1.6f;
-				fireVel -= shotOrigin;
-				//fireVel = Vector3.Normalize(fireVel);
-				//fireVel *= speedConstant;
+								GameObject curFireBall = (GameObject)Instantiate (FireBall, shotOrigin, Quaternion.identity);
+								FireVelController _FireBall = curFireBall.GetComponent<FireVelController> ();
+
+								Vector3 fireVel = PE_Controller.instance.transform.position;
+								fireVel.y += 1.6f;
+								fireVel -= shotOrigin;
+
+								if (Mathf.Abs (fireVel.x) < minDist) {
+
+										if (fireVel.x > 0) {
+												fireVel.x = minDist;
+										} else {
+												fireVel.x = -1 * minDist;
+										}
+								}
+
+								//fireVel = Vector3.Normalize(fireVel);
+								//fireVel *= speedConstant;
 
 
 
-				_FireBall.fireSpeed = fireVel;
-			}
-		}
+								_FireBall.fireSpeed = fireVel;
+								alreadyShot = true;
+						}
+				} else {
+					alreadyShot = false;
+				}
 	
 	}
 }
