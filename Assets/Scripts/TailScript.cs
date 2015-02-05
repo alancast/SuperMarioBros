a75c26fd8ef32e17528d45975c85fdaecb909d00
Alex <alancast@umich.edu>
2015-02-04 20:53:17 -0500

    almost done with tail

---
+++ Assets/Scripts/TailScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TailScript : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag != "Goomba") return;
+		print ("trigged");
+	}
+	
+	void OnTriggerStay(Collider other){
+		OnTriggerEnter(other);
+	}
+	
+	void OnDrawGizmos() {
+		Gizmos.color = Color.yellow;
+		Gizmos.DrawWireCube(transform.position, transform.localScale);
+	}
+}
